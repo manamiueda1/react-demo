bb913e65a856588fd3a029b5106371ba8de378fc
Manami Ueda <manamiueda1@gmail.com>
2021-02-16 12:25:06 -0500

    rendering values inside object

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+let pageContent = {
+  title: "React Notes",
+  paragraph1:
+    "a library is a collection of non-volatile resources used by computer programs, often for software development.",
+  paragraph2:
+    "These may include configuration data, documentation, help data, message templates, pre-written code and subroutines, classes, values or type specifications.",
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{pageContent.title}</h1>
+      <p>{pageContent.paragraph1}</p>
+      <p>{pageContent.paragraph2}</p>
     </div>
   );
 }
