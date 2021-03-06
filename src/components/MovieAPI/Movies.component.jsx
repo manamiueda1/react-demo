763b5072b35a1b53288476251a9e904afbdae953
Manami Ueda <manamiueda1@gmail.com>
2021-03-06 13:19:41 -0500

    added movies component

--- src/components/MovieAPI/Movies.component.jsx
+++ src/components/MovieAPI/Movies.component.jsx
@@ -11,13 +11,24 @@ class Movies extends Component {
 
 
     componentDidMount(){
+        fetch(`https://api.themoviedb.org/3/list/1?api_key=67095d224daa8dc607d145fc46fb7c47`)
+            .then(data => data.json())
+            .then(response => response)
+            .then(data => this.setState({movies: data.items}))
     }
 
-    render(){
+    // componentDidMount(){
+    //     fetch(`https://api.themoviedb.org/3/movie/551?api_key=67095d224daa8dc607d145fc46fb7c47`)
+    //         .then(data => data.json())
+    //         .then(response => console.log(response))
+    // }
 
+    render(){
+console.log(this.state.movies)
         return(
             <div>
                 <h1>Movies</h1>
+                {this.state.movies.map(movie => <p>{movie.title}</p>)}
             </div>
         )
     }
